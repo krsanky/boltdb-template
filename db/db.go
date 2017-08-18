58d08ffae33a78adb262d41c0cc211b70ba4ee92
paul wisehart <paul@oldcode.org>
2017-08-18 16:40:10 -0400

    1st commit

---
+++ db/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/boltdb/bolt"
+	"github.com/krsanky/config"
+)
+
+var DB *bolt.DB
+
+func init() {
+	var err error
+	var db_file = config.Get("db_file")
+	fmt.Printf("db_file:%s\n", db_file)
+	DB, err = bolt.Open(db_file, 0600, &bolt.Options{Timeout: 5 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
